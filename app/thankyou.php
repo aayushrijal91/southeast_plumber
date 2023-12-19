66f0aab5824039de5e17017ae4f3d1fac9c552aa
Aayush Rijal <arijal@aiims.com.au>
2023-12-19 19:05:06 +1100

    Ready for live

---
+++ app/thankyou.php
@@ -0,0 +1,19 @@
+<?php include __DIR__ . '/functions.php'; ?>
+
+<?php include __DIR__ . '/src/header.php'; ?>
+
+<!-- Event snippet for Booking Form conversion page -->
+
+<script>
+    gtag('event', 'conversion', {
+        'send_to': 'AW-11452255326/98nfCM-D9IAZEN6Y7tQq'
+    });
+</script>
+
+<div class="thankyou-wrapper text-white text-center py-3" style="background-color: green;">
+    Thank you! Your message has been sent. We will contact you shortly.
+</div>
+
+<?php include __DIR__ . '/src/main.php'; ?>
+
+<?php include __DIR__ . '/src/footer.php'; ?>
